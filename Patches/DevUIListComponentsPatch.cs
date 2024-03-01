2cc77f5a8389d335f9486638f49a9d539578d935
Nick Morrison <nickmorrison09@gmail.com>
2024-02-29 21:17:56 -0500

    Fixed it, gonna simplify the code

--- Patches/DevUIListComponentsPatch.cs
+++ Patches/DevUIListComponentsPatch.cs
@@ -1,12 +1,17 @@
 using System;
+using Game.Buildings;
 using Game.Common;
+using Game.Net;
 using Game.Objects;
 using Game.Rendering;
+using Game.Simulation;
+using Game.Tools;
 using Game.UI.InGame;
 using Game.Vehicles;
 using HarmonyLib;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Entities.UniversalDelegates;
 
 namespace CS2ModsTesting {
 
@@ -58,17 +63,27 @@ namespace CS2ModsTesting {
                 updateInfoMethod
             ));
 
+            EntityQueryBuilder eqb = new EntityQueryBuilder(Allocator.Temp);
+            var streetLightSystemQuery = eqb.WithAll<StreetLight, LightState, Emissive>()
+                .WithNone<Road, Building, Watercraft, Deleted, Destroyed, Temp, Owner>()
+                .Build(em);
+            eqb.Dispose();
             updateInfoMethod = (Entity entity, Entity prefab, InfoList info) => {
                 var streetLight = em.GetComponentData<StreetLight>(entity);                
                 var streetLightStates = em.GetBuffer<LightState>(entity, true);
+                var emissives = em.GetBuffer<Emissive>(entity, true);
                 info.label = "Street Light Info";
                 info.Add(new InfoList.Item($"Street light State: {streetLight.m_State.ToString()}"));                
                 foreach(LightState state in streetLightStates) {
-                    info.Add(new InfoList.Item($"Intensity: {state.m_Intensity}, Colour: {state.m_Color}"));
+                    info.Add(new InfoList.Item($"LightState Intensity: {state.m_Intensity}, Colour: {state.m_Color}"));
                 }
+                foreach(Emissive emissive in emissives) {
+                    info.Add(new InfoList.Item($"Emissive Intensity: {emissive.m_LightOffset}, Colour: {emissive.m_Updated}"));
+                }        
+                info.Add(new InfoList.Item($"Matches Query: {streetLightSystemQuery.Matches(entity)}"));
             };
             ___m_InfoUISystem.AddDeveloperInfo(new InfoList(
-                (entity, prefab) => em.HasComponent<StreetLight>(entity) && em.HasBuffer<LightState>(entity),
+                (entity, prefab) => em.HasComponent<StreetLight>(entity) && em.HasBuffer<LightState>(entity) && em.HasBuffer<Emissive>(entity),
                 updateInfoMethod
             ));
         }
--- Systems/PropLightingSystem.cs
+++ Systems/PropLightingSystem.cs
@@ -25,7 +25,8 @@ namespace CS2ModsTesting.Systems {
         SimulationSystem simulationSystem;
 
         ComponentTypeHandle<StreetLight> streetLightTypeHandle;
-        // ComponentTypeHandle<LightState> lightStateTypeHandle;
+        BufferTypeHandle<LightState> lightStateTypeHandle;
+        BufferTypeHandle<Emissive> emissiveTypeHandle;
         EntityTypeHandle entityTypeHandle;
         ComponentTypeHandle<PseudoRandomSeed> pseudoRandomSeedHandle;
         
@@ -34,9 +35,9 @@ namespace CS2ModsTesting.Systems {
         {
             base.OnCreate();
             EntityQueryBuilder eqb = new EntityQueryBuilder(Allocator.Temp);
-            propQuery = eqb.WithAll<UpdateFrame, StreetLight, LightState>()
+            propQuery = eqb.WithAll<StreetLight, LightState, Emissive>()
                 .WithNone<Road, Building, Watercraft, Deleted, Destroyed, Temp, Owner>()
-                .Build(this.EntityManager);
+                .Build(this.EntityManager);            
             lightingSystem = World.GetOrCreateSystemManaged<LightingSystem>();
             endFrameBarrier = World.GetOrCreateSystemManaged<EndFrameBarrier>();
             simulationSystem = World.GetOrCreateSystemManaged<SimulationSystem>();
@@ -44,6 +45,8 @@ namespace CS2ModsTesting.Systems {
             streetLightTypeHandle = GetComponentTypeHandle<StreetLight>();
             entityTypeHandle = GetEntityTypeHandle();
             pseudoRandomSeedHandle = GetComponentTypeHandle<PseudoRandomSeed>();
+            emissiveTypeHandle = GetBufferTypeHandle<Emissive>();
+            lightStateTypeHandle = GetBufferTypeHandle<LightState>();
 
             eqb.Dispose();
             base.RequireForUpdate(propQuery);
@@ -51,16 +54,21 @@ namespace CS2ModsTesting.Systems {
 
         protected override void OnUpdate()
         {
-            this.propQuery.ResetFilter();
-            this.propQuery.SetSharedComponentFilter<UpdateFrame>(new UpdateFrame(SimulationUtils.GetUpdateFrameWithInterval(this.simulationSystem.frameIndex, (uint)this.GetUpdateInterval(SystemUpdatePhase.GameSimulation), 16)));
+            // this.propQuery.ResetFilter();
+            // this.propQuery.SetSharedComponentFilter<UpdateFrame>(new UpdateFrame(SimulationUtils.GetUpdateFrameWithInterval(this.simulationSystem.frameIndex, (uint)this.GetUpdateInterval(SystemUpdatePhase.GameSimulation), 16)));
 
             entityTypeHandle.Update(this);
             streetLightTypeHandle.Update(this);
+            pseudoRandomSeedHandle.Update(this);     
+            emissiveTypeHandle.Update(this);
+            lightStateTypeHandle.Update(this);       
 
             UpdateLightsJob job = new() {
                 entityTypeHandle = entityTypeHandle,
                 streetLightTypeHandle = streetLightTypeHandle,
                 pseudoRandomHandle = pseudoRandomSeedHandle,
+                emissiveTypeHandle = emissiveTypeHandle,
+                lightStateTypeHandle = lightStateTypeHandle,
                 ecb = endFrameBarrier.CreateCommandBuffer().AsParallelWriter(),
                 brightness = Mathf.RoundToInt(this.lightingSystem.dayLightBrightness * 1000f)
             };
@@ -74,7 +82,8 @@ namespace CS2ModsTesting.Systems {
             public EntityTypeHandle entityTypeHandle;
             public ComponentTypeHandle<StreetLight> streetLightTypeHandle;
             public ComponentTypeHandle<PseudoRandomSeed> pseudoRandomHandle;
-            // public ComponentTypeHandle<LightState> lightStateTypeHandle;
+            public BufferTypeHandle<LightState> lightStateTypeHandle;
+            public BufferTypeHandle<Emissive> emissiveTypeHandle;
 
             public int brightness;
 
@@ -84,19 +93,35 @@ namespace CS2ModsTesting.Systems {
             {
                 NativeArray<Entity> entities = chunk.GetNativeArray(this.entityTypeHandle);
                 NativeArray<StreetLight> streetLights = chunk.GetNativeArray(ref this.streetLightTypeHandle);
-                NativeArray<PseudoRandomSeed> pseudoRandomSeeds = chunk.GetNativeArray(ref this.pseudoRandomHandle);
-                // NativeArray<LightState> lightStates = chunk.GetNativeArray(ref this.lightStateTypeHandle);
+                NativeArray<PseudoRandomSeed> pseudoRandomSeeds = chunk.GetNativeArray(ref this.pseudoRandomHandle);                
+                var lightStatesAccessor = chunk.GetBufferAccessor(ref this.lightStateTypeHandle);
+                var emissivesAccessor = chunk.GetBufferAccessor(ref this.emissiveTypeHandle);
                 for(int i = 0; i < entities.Length; i++) {
                     Unity.Mathematics.Random random = pseudoRandomSeeds[i].GetRandom((uint)PseudoRandomSeed.kBrightnessLimit);
                     ref var streetLight = ref streetLights.ElementAt(i);
-                    bool isDark = this.brightness < random.NextInt(200, 300);
-                    if (isDark && streetLight.m_State == StreetLightState.TurnedOff) {
-                        streetLight.m_State = StreetLightState.None;
+                    var lightStates = lightStatesAccessor[i];
+                    var emissives = emissivesAccessor[i];
+                    bool isDark = this.brightness < random.NextInt(200, 300);                    
+                    bool updateEmissives = false;
+                    if (isDark && streetLight.m_State == StreetLightState.TurnedOff) {                        
+                        streetLight.m_State = StreetLightState.None;                        
                         this.ecb.AddComponent<EffectsUpdated>(unfilteredChunkIndex, entities[i]);
+                        // for(int j = 0; j < lightStates.Length; j++) {
+                        //     ref var state = ref lightStates.ElementAt(j);
+                        //     state.m_Intensity = 0;
+                        // }
+                        updateEmissives = false;   
                     }
                     else if (!isDark && streetLight.m_State == StreetLightState.None) {
                         streetLight.m_State = StreetLightState.TurnedOff;
                         this.ecb.AddComponent<EffectsUpdated>(unfilteredChunkIndex, entities[i]);
+                        updateEmissives = true;
+                    }
+                    if(updateEmissives) {
+                        for(int j = 0; j < emissives.Length; j++) {
+                            ref var emis = ref emissives.ElementAt(j);
+                            emis.m_Updated = true;
+                        }
                     }
                 }
                 
